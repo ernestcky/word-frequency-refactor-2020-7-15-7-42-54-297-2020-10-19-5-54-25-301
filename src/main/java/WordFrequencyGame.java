8cc5d6796385bcf639a2997a23809eac00ec88a3
Ernest Cheung <ernest.cheung@oocl.com>
2020-11-30 17:26:12 +0800

    [WordFrequencyGame][Ernest] Removed duplicated logic for handling sentence with length == 1

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -11,38 +11,34 @@ public class WordFrequencyGame {
     public static final String EXCEPTION_CALCULATE_ERROR = "Calculate Error";
 
     public String getResult(String sentence) {
-        if (sentence.split(REGEX_WHITE_SPACE).length == 1) {
-            return sentence + " 1";
-        } else {
-            try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split(REGEX_WHITE_SPACE);
+        try {
+            //split the input string with 1 to n pieces of spaces
+            String[] words = sentence.split(REGEX_WHITE_SPACE);
 
-                List<WordFrequency> wordFrequencyList = new ArrayList<>();
-                for (String word : words) {
-                    wordFrequencyList.add(new WordFrequency(word, 1));
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordFrequency>> wordCountMap = getListMap(wordFrequencyList);
-
-                List<WordFrequency> wordCountList = new ArrayList<>();
-                for (Map.Entry<String, List<WordFrequency>> entry : wordCountMap.entrySet()) {
-                    wordCountList.add(new WordFrequency(entry.getKey(), entry.getValue().size()));
-                }
-                wordFrequencyList = wordCountList;
-
-                wordFrequencyList.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
-
-                StringJoiner wordFrequencyResult = new StringJoiner(LINE_ESCAPER);
-                for (WordFrequency wordFrequency : wordFrequencyList) {
-                    String wordFrequencyLine = wordFrequency.getWord() + " " + wordFrequency.getWordCount();
-                    wordFrequencyResult.add(wordFrequencyLine);
-                }
-                return wordFrequencyResult.toString();
-            } catch (Exception exception) {
-                return EXCEPTION_CALCULATE_ERROR;
+            List<WordFrequency> wordFrequencyList = new ArrayList<>();
+            for (String word : words) {
+                wordFrequencyList.add(new WordFrequency(word, 1));
             }
+
+            //get the map for the next step of sizing the same word
+            Map<String, List<WordFrequency>> wordCountMap = getListMap(wordFrequencyList);
+
+            List<WordFrequency> wordCountList = new ArrayList<>();
+            for (Map.Entry<String, List<WordFrequency>> entry : wordCountMap.entrySet()) {
+                wordCountList.add(new WordFrequency(entry.getKey(), entry.getValue().size()));
+            }
+            wordFrequencyList = wordCountList;
+
+            wordFrequencyList.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
+
+            StringJoiner wordFrequencyResult = new StringJoiner(LINE_ESCAPER);
+            for (WordFrequency wordFrequency : wordFrequencyList) {
+                String wordFrequencyLine = wordFrequency.getWord() + " " + wordFrequency.getWordCount();
+                wordFrequencyResult.add(wordFrequencyLine);
+            }
+            return wordFrequencyResult.toString();
+        } catch (Exception exception) {
+            return EXCEPTION_CALCULATE_ERROR;
         }
     }
 
